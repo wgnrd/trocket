841b72b60e5a86232f6462734e703f8513236c61
Wagner <dominik.waagna@gmail.com>
2021-08-04 08:52:57 +0200

    remove unused imports

--- src/index.js
+++ src/index.js
@@ -3,13 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-  useQuery,
-  gql,
-} from '@apollo/client';
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   useQuery,
+//   gql,
+// } from '@apollo/client';
 
 ReactDOM.render(
   <React.StrictMode>
