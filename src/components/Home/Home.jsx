4a57a3acf9b9dc78879b7253fc8e62927fe65542
wgnrd <dominik.waagna@gmail.com>
2021-09-07 17:16:40 +0200

    first draft of prefilled searchbar

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,30 +1,33 @@
 import React from 'react';
-import { useQuery } from '@apollo/client';
+import { useLazyQuery } from '@apollo/client';
 import Searchbar from './Searchbar';
 import { COUNTRY_BY_CODE } from '../../graphQl/Queries';
 import { useState } from 'react';
 
 function Home() {
-  const [searchKey, setSearchKey] = useState('AT');
+  const [searchKey, setSearchKey] = useState('');
 
-  const { data, loading, error, refetch } = useQuery(COUNTRY_BY_CODE, {
-    variables: { code: searchKey },
-  });
+  const [getCountryByCode, { data, loading, called }] =
+    useLazyQuery(COUNTRY_BY_CODE);
 
   const onSearchChange = (event) => {
     if (event.target.value.length < 2) return;
 
     setSearchKey(event.target.value.toUpperCase());
-    refetch();
+    console.log(event.target.value.toUpperCase());
+    getCountryByCode({
+      variables: { code: searchKey },
+    });
   };
 
   // if (loading) return <h1>Loading...</h1>;
-  if (error) return <h1>ERROR</h1>;
+  // if (error) return <h1>ERROR</h1>;
 
   return (
     <div>
       <Searchbar onChange={onSearchChange} />
       {!loading &&
+        data &&
         data.countries.map((country) => (
           <div key={country.id}>
             <h2>{country.__typename}</h2>
--- src/components/Home/Searchbar.jsx
+++ src/components/Home/Searchbar.jsx
@@ -1,12 +1,18 @@
+import { useQuery } from '@apollo/client';
+import { CLIENT_COUNTRY_CODE } from '../../graphQl/Queries';
 import React from 'react';
 
 const Searchbar = ({ onChange }) => {
+  const { data, loading, error } = useQuery(CLIENT_COUNTRY_CODE);
+
+  if (loading) return <h1>Loading...</h1>;
   return (
     <input
       type="text"
       name="searchbar"
       id="sbAlphaCode"
       placeholder="Top level domain"
+      value={data.client.ipAddress.country.alpha2Code}
       maxLength="2"
       onChange={onChange}
     />
