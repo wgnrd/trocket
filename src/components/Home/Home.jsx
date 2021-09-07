db56187ee42f64f6514f2e7eaaefa743202338eb
Wagner <dominik.waagna@gmail.com>
2021-09-07 18:50:19 +0200

    searchbar is prefilled by client country

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,21 +2,15 @@ import React from 'react';
 import { useLazyQuery } from '@apollo/client';
 import Searchbar from './Searchbar';
 import { COUNTRY_BY_CODE } from '../../graphQl/Queries';
-import { useState } from 'react';
 
 function Home() {
-  const [searchKey, setSearchKey] = useState('');
+  const [getCountryByCode, { data, loading }] = useLazyQuery(COUNTRY_BY_CODE);
 
-  const [getCountryByCode, { data, loading, called }] =
-    useLazyQuery(COUNTRY_BY_CODE);
+  const onSearchChange = (searchInput) => {
+    if (searchInput.length < 2) return;
 
-  const onSearchChange = (event) => {
-    if (event.target.value.length < 2) return;
-
-    setSearchKey(event.target.value.toUpperCase());
-    console.log(event.target.value.toUpperCase());
     getCountryByCode({
-      variables: { code: searchKey },
+      variables: { code: searchInput },
     });
   };
 
@@ -26,17 +20,23 @@ function Home() {
   return (
     <div>
       <Searchbar onChange={onSearchChange} />
-      {!loading &&
-        data &&
-        data.countries.map((country) => (
-          <div key={country.id}>
-            <h2>{country.__typename}</h2>
-            <p>
-              <b>Name: </b>
-              {country.name}
-            </p>
-          </div>
-        ))}
+      {
+        // TODO - fix this conditional mess with a new component
+        loading ? (
+          <h2>Loading...</h2>
+        ) : (
+          data &&
+          data.countries.map((country) => (
+            <div key={country.id}>
+              <h2>{country.__typename}</h2>
+              <p>
+                <b>Name: </b>
+                {country.name}
+              </p>
+            </div>
+          ))
+        )
+      }
     </div>
   );
 }
--- src/components/Home/Searchbar.jsx
+++ src/components/Home/Searchbar.jsx
@@ -1,20 +1,37 @@
 import { useQuery } from '@apollo/client';
 import { CLIENT_COUNTRY_CODE } from '../../graphQl/Queries';
-import React from 'react';
+import React, { useState } from 'react';
 
 const Searchbar = ({ onChange }) => {
   const { data, loading, error } = useQuery(CLIENT_COUNTRY_CODE);
+  const [value, setValue] = useState();
 
-  if (loading) return <h1>Loading...</h1>;
+  if (error) return <h1>Error..</h1>;
+  if (loading) return <input disabled />;
+
+  const onSearchChange = ({ target }) => {
+    // switch case of new value
+    setValue(target.value.toUpperCase());
+
+    // push the value up
+    onChange(target.value.toUpperCase());
+  };
+
+  // if the value of input field is undefined, prefill it with the countrycode of the client,
+  // this also triggers the first query of the country itself in another component
+  if (value === undefined) {
+    setValue(data.client.ipAddress.country.alpha2Code.toUpperCase());
+  }
+
   return (
     <input
       type="text"
       name="searchbar"
       id="sbAlphaCode"
-      placeholder="Top level domain"
-      value={data.client.ipAddress.country.alpha2Code}
+      placeholder={'e.g. RU'}
       maxLength="2"
-      onChange={onChange}
+      value={value}
+      onChange={onSearchChange}
     />
   );
 };
